e02c65c9a330400a7437a0dbd8500d0308bf5184
Cenk Gündoğan <mail@cgundogan.de>
2016-11-06 10:52:39 +0100

    use tmpfs for /tmp

--- build_local.sh
+++ build_local.sh
@@ -19,7 +19,7 @@ _docker() {
         -e 'GIT_CACHE_DIR=/data/gitcache' \
         -v '/home/ccache:/data/ccache' \
         -v '/srv/riot-ci/.gitcache:/data/gitcache' \
-        -v '/tmp:/tmp' \
+        --tmpfs '/tmp' \
         -w '/data/riotbuild' \
         --security-opt seccomp=unconfined \
         'riot/riotbuild:latest' $* &
