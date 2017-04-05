92e39f1af529ce7bec396f5e7538b9b80598c169
Kaspar Schleiser <kaspar@schleiser.de>
2017-04-05 10:19:25 +0200

    build.sh: use common.sh, BASEDIR, show merge commit errors, run post_build scripts

--- build.sh
+++ build.sh
@@ -2,38 +2,14 @@
 
 ACTION="$1"
 
+MERGE_COMMIT_REPO="kaspar030/RIOT"
+
+BASEDIR="$(dirname $(realpath $0))"
+
+. "${BASEDIR}/common.sh"
+
 random() {
     hexdump -n ${1:-4} -e '/2 "%u"' /dev/urandom
-}
-
-# get org/repo from https://github.com/org/repo.git
-repo_path() {
-    local repo_url=$1
-    echo $(basename $(dirname $repo_url))/$(basename $repo_url .git)
-}
-
-_gethead() {
-    local gitdir="$1"
-    local url="$2"
-    local branch="${3:-master}"
-
-    git -C "$gitdir" ls-remote "$url" "${branch}" | cut -f1
-}
-
-gethead() {
-    local url="$1"
-    local branch="${2:-master}"
-
-    local gitdir="$(git rev-parse --show-toplevel 2>/dev/null)"
-    [ -z "$gitdir" ] && {
-        local tmpdir="$(mktemp -d)"
-        gitdir="$tmpdir"
-    }
-    _gethead "$gitdir" "$url" "$branch"
-
-    RES=$?
-    [ -n "$tmpdir" ] && rm -rf "$tmpdir"
-    exit $RES
 }
 
 retry() {
@@ -66,14 +42,15 @@ create_merge_commit() {
     local tmpdir="$(mktemp -d /tmp/murdock_git.XXXXXX)"
 
     MERGE_BRANCH=pull/$base_head/$pr_head
-    (
+    set +e
+    OUT="$({
         set -e
         echo "--- cloning base repo"
         git-cache clone $base_repo $base_head $tmpdir
         git -C $tmpdir checkout
 
         echo "--- adding remotes"
-        git -C $tmpdir remote add cache_repo "git@github.com:murdock-ci/RIOT"
+        git -C $tmpdir remote add cache_repo "git@github.com:$MERGE_COMMIT_REPO"
         git -C $tmpdir remote add pr_repo "$pr_repo"
 
         echo "--- checking out merge branch"
@@ -89,7 +66,13 @@ create_merge_commit() {
         }
         echo "--- pushing result"
         git -C $tmpdir push --force cache_repo
-    ) > /dev/null 2>&1
+        } 2>&1 )"
+    RES=$?
+    set -e
+    [ $RES -ne 0 ] && {
+        echo "$OUT"
+        false
+    }
 
     export CI_MERGE_COMMIT="$(git -C $tmpdir rev-parse $MERGE_BRANCH)"
     rm -rf $tmpdir
@@ -118,7 +101,7 @@ case "$ACTION" in
 
         create_merge_commit $CI_BASE_REPO $CI_BASE_COMMIT $CI_PULL_REPO $CI_PULL_BRANCH $CI_PULL_COMMIT $CI_PULL_NR
 
-        export DWQ_REPO="https://github.com/murdock-ci/RIOT"
+        export DWQ_REPO="https://github.com/$MERGE_COMMIT_REPO"
         export DWQ_COMMIT="${CI_MERGE_COMMIT}"
 
         echo "---- using merge commit SHA1=${CI_MERGE_COMMIT}"
@@ -133,7 +116,7 @@ case "$ACTION" in
 
         REPORT_QUEUE="status::PR${CI_PULL_NR}:$(random)"
 
-        /srv/murdock/murdock/parse_result.py "$REPORT_QUEUE" $CI_PULL_NR &
+        $BASEDIR/parse_result.py "$REPORT_QUEUE" $CI_PULL_NR &
         REPORTER=$!
 
         set +e
@@ -145,7 +128,10 @@ case "$ACTION" in
 
         RES=$?
 
-        kill $REPORTER >/dev/null 2>&1 || true
+        kill $REPORTER >/dev/null 2>&1 && wait $REPORTER 2>/dev/null
+
+        # run post-build.d scripts
+        post_build
 
         exit $RES
         ;;
@@ -153,7 +139,7 @@ case "$ACTION" in
         {
             cat output.txt
             echo ""
-            [ -s result.json ] && HTML=1 python3 /srv/murdock/murdock/parse_result.py result.json
+            [ -s result.json ] && HTML=1 ${BASEDIR}/parse_result.py result.json
         } | ansi2html -s solarized -u > output.html
         ;;
     *)
