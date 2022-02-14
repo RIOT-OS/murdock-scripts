3f243ab198141e85e81d3b5ee20736a04c431e87
Alexandre Abadie <alexandre.abadie@inria.fr>
2022-02-14 10:47:07 +0100

    build.sh: refactor to adapt to new backend workflow (no separated action)

--- build.sh
+++ build.sh
@@ -120,119 +120,106 @@ create_merge_commit() {
     echo "--- done."
 }
 
-case "$ACTION" in
-    run)
-        # clean possible output
-        rm -Rf output/
-        rm -f prstatus.html.snip result.json
+: ${NIGHTLY:=0}
+: ${STATIC_TESTS:=0}
+: ${APPS:=}
+: ${BOARDS:=}
 
-        STATUS='{"status" : {"status": "Fetching code"}}'
-        /usr/bin/curl -s -d "${STATUS}" -H "Content-Type: application/json" -H "Authorization: ${CI_JOB_TOKEN}" -X PUT http://localhost:8000/jobs/running/${CI_JOB_UID}/status > /dev/null
+main() {
+    local status='{"status" : {"status": "Fetching code"}}'
+    /usr/bin/curl -s -d "${status}" -H "Content-Type: application/json" -H "Authorization: ${CI_JOB_TOKEN}" -X PUT http://localhost:8000/jobs/running/${CI_JOB_UID}/status > /dev/null
 
-        : ${NIGHTLY:=0}
-        : ${STATIC_TESTS:=0}
-        : ${APPS:=}
-        : ${BOARDS:=}
+    export APPS BOARDS
 
-        export APPS BOARDS
-
-        if [ -n "${CI_BUILD_COMMIT}" ]; then
-            if [ -n "${CI_BUILD_BRANCH}" ]; then
-                echo "-- Building branch ${CI_BUILD_BRANCH} head: ${CI_BUILD_COMMIT}..."
-                QUEUE_NAME=${CI_BUILD_BRANCH}${CI_BUILD_COMMIT}
-                # Create a symlink to this job so it can be linked easily in an URL
-                mkdir -p ../branches
-                rm -f ../branches/${CI_BUILD_BRANCH}
-                ln -s ${PWD} ../branches/${CI_BUILD_BRANCH}
-            elif [ -n "${CI_BUILD_TAG}" ]; then
-                echo "-- Building tag ${CI_BUILD_TAG} (${CI_BUILD_COMMIT})..."
-                QUEUE_NAME=${CI_BUILD_TAG}${CI_BUILD_COMMIT}
-                # Create a symlink to this job so it can be linked easily in an URL
-                mkdir -p ../tags
-                rm -f ../tags/${CI_BUILD_TAG}
-                ln -s ${PWD} ../tags/${CI_BUILD_TAG}
-            else
-                echo "-- Building commit ${CI_BUILD_COMMIT}..."
-                QUEUE_NAME=${CI_BUILD_COMMIT}
-            fi
-
-            REPORT_QUEUE="status::${QUEUE_NAME}:$(random)"
-            export NIGHTLY STATIC_TESTS
-            export DWQ_REPO="${CI_BUILD_REPO}"
-            export DWQ_COMMIT="${CI_BUILD_COMMIT}"
-            export DWQ_ENV="-E APPS -E BOARDS -E NIGHTLY -E STATIC_TESTS"
-        else # Building a PR
-
-            echo "-- github reports HEAD of ${CI_BASE_BRANCH} as $CI_BASE_COMMIT"
-
-            ACTUAL_BASE_HEAD="$(gethead ${CI_BASE_REPO} ${CI_BASE_BRANCH})"
-            if [ -n "$ACTUAL_BASE_HEAD" ]; then
-                if [ "$ACTUAL_BASE_HEAD" != "$CI_BASE_COMMIT" ]; then
-                    echo "-- HEAD of ${CI_BASE_BRANCH} is $ACTUAL_BASE_HEAD"
-                    export CI_BASE_COMMIT="$ACTUAL_BASE_HEAD"
-                fi
-            fi
-
-            create_merge_commit $CI_BASE_REPO $CI_BASE_COMMIT $CI_PULL_REPO $CI_PULL_COMMIT $CI_PULL_NR
-
-            export DWQ_REPO="${CI_GIT_URL_WORKER}/${MERGE_COMMIT_REPO}"
-            export DWQ_COMMIT="${CI_MERGE_COMMIT}"
-
-            echo "---- using merge commit SHA1=${CI_MERGE_COMMIT}"
-
-            dwqc "test -x .murdock" || {
-                echo "PR does not contain .murdock build script, please rebase!"
-                rm -f result.json
-                exit 2
-            }
-
-            echo "-- Building PR#$CI_PULL_NR $CI_PULL_URL head: $CI_PULL_COMMIT..."
-
-            export DWQ_ENV="-E CI_BASE_REPO -E CI_BASE_BRANCH -E CI_PULL_REPO -E CI_PULL_COMMIT \
-                -E CI_PULL_NR -E CI_PULL_URL -E CI_PULL_LABELS -E CI_MERGE_COMMIT \
-                -E CI_BASE_COMMIT -E APPS -E BOARDS -E NIGHTLY -E STATIC_TESTS"
-
-            REPORT_QUEUE="status::PR${CI_PULL_NR}:$(random)"
-        fi
-
-        python $BASEDIR/reporter.py "$REPORT_QUEUE" $CI_JOB_UID $CI_JOB_TOKEN &
-        REPORTER=$!
-
-        set +e
-
-        get_jobs | dwqc ${DWQ_ENV} \
-            --maxfail 500 \
-            --quiet --report $REPORT_QUEUE --outfile result.json
-
-        RES=$?
-
-        sleep 1
-
-        kill $REPORTER >/dev/null 2>&1 && wait $REPORTER 2>/dev/null
-
-        # export result to post-build scripts
-        if [ $RES -eq 0 ]; then
-            export CI_BUILD_RESULT=success
+    if [ -n "${CI_BUILD_COMMIT}" ]; then
+        if [ -n "${CI_BUILD_BRANCH}" ]; then
+            echo "-- Building branch ${CI_BUILD_BRANCH} head: ${CI_BUILD_COMMIT}..."
+            # Create a symlink to this job so it can be linked easily in an URL
+            mkdir -p ../branches
+            rm -f ../branches/${CI_BUILD_BRANCH}
+            ln -s ${PWD} ../branches/${CI_BUILD_BRANCH}
+        elif [ -n "${CI_BUILD_TAG}" ]; then
+            echo "-- Building tag ${CI_BUILD_TAG} (${CI_BUILD_COMMIT})..."
+            # Create a symlink to this job so it can be linked easily in an URL
+            mkdir -p ../tags
+            rm -f ../tags/${CI_BUILD_TAG}
+            ln -s ${PWD} ../tags/${CI_BUILD_TAG}
         else
-            export CI_BUILD_RESULT=failed
+            echo "-- Building commit ${CI_BUILD_COMMIT}..."
         fi
 
-        # run post-build.d scripts
-        post_build
+        export NIGHTLY STATIC_TESTS
+        export DWQ_REPO="${CI_BUILD_REPO}"
+        export DWQ_COMMIT="${CI_BUILD_COMMIT}"
+        export DWQ_ENV="-E APPS -E BOARDS -E NIGHTLY -E STATIC_TESTS"
+    else # Building a PR
 
-        if [ -n "${NOTIFY_RESULTS}" ] && [ -n "${CI_BUILD_BRANCH}" ]; then
-            # trigger result notifier on branches
-            echo "--- Notify results for branch ${CI_BUILD_BRANCH}"
-            python ${BASEDIR}/notify.py "${CI_BUILD_BRANCH}" --job-uid "${CI_JOB_UID}"
+        echo "-- github reports HEAD of ${CI_BASE_BRANCH} as $CI_BASE_COMMIT"
+
+        local actual_base_head="$(gethead ${CI_BASE_REPO} ${CI_BASE_BRANCH})"
+        if [ -n "$ACTUAL_BASE_HEAD" ]; then
+            if [ "$ACTUAL_BASE_HEAD" != "$CI_BASE_COMMIT" ]; then
+                echo "-- HEAD of ${CI_BASE_BRANCH} is ${actual_base_head}"
+                export CI_BASE_COMMIT="${actual_base_head}"
+            fi
         fi
 
-        exit $RES
-        ;;
-    finalize)
-        python ${BASEDIR}/process_result.py
-        ;;
-    *)
-        echo "$0: unhandled action $ACTION"
-        exit 1
-        ;;
-esac
+        create_merge_commit $CI_BASE_REPO $CI_BASE_COMMIT $CI_PULL_REPO $CI_PULL_COMMIT $CI_PULL_NR
+
+        export DWQ_REPO="${CI_GIT_URL_WORKER}/${MERGE_COMMIT_REPO}"
+        export DWQ_COMMIT="${CI_MERGE_COMMIT}"
+
+        echo "---- using merge commit SHA1=${CI_MERGE_COMMIT}"
+
+        dwqc "test -x .murdock" || {
+            echo "PR does not contain .murdock build script, please rebase!"
+            rm -f result.json
+            exit 2
+        }
+
+        echo "-- Building PR#$CI_PULL_NR $CI_PULL_URL head: $CI_PULL_COMMIT..."
+
+        export DWQ_ENV="-E CI_BASE_REPO -E CI_BASE_BRANCH -E CI_PULL_REPO -E CI_PULL_COMMIT \
+            -E CI_PULL_NR -E CI_PULL_URL -E CI_PULL_LABELS -E CI_MERGE_COMMIT \
+            -E CI_BASE_COMMIT -E APPS -E BOARDS -E NIGHTLY -E STATIC_TESTS"
+    fi
+
+    local report_queue="status::${CI_JOB_UID}:$(random)"
+    python ${BASEDIR}/reporter.py "${report_queue}" ${CI_JOB_UID} ${CI_JOB_TOKEN} &
+    local reporter_pid=$!
+
+    set +e
+
+    get_jobs | dwqc ${DWQ_ENV} \
+        --maxfail 500 \
+        --quiet --report ${REPORT_QUEUE} --outfile result.json
+
+    RES=$?
+
+    sleep 1
+
+    kill ${reporter_pid} >/dev/null 2>&1 && wait ${reporter_pid} 2>/dev/null
+
+    # export result to post-build scripts
+    if [ $RES -eq 0 ]; then
+        export CI_BUILD_RESULT=success
+    else
+        export CI_BUILD_RESULT=failed
+    fi
+
+    # run post-build.d scripts
+    post_build
+
+    # Process result.json for UI
+    python ${BASEDIR}/process_result.py
+
+    if [ -n "${NOTIFY_RESULTS}" ] && [ -n "${CI_BUILD_BRANCH}" ]; then
+        # trigger result notifier on branches
+        echo "--- Notify results for branch ${CI_BUILD_BRANCH}"
+        python ${BASEDIR}/notify.py "${CI_BUILD_BRANCH}" --job-uid "${CI_JOB_UID}"
+    fi
+
+    exit $RES
+}
+
+main
